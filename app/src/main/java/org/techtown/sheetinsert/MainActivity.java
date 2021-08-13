6c13be7fa611c12bc36aa732894c64fc9b3a2b3f
shinyumi43 <yumishin43@gmail.com>
2021-08-13 12:57:52 +0900

    sheet-insert

---
+++ app/src/main/java/org/techtown/sheetinsert/MainActivity.java
@@ -0,0 +1,32 @@
+package org.techtown.sheetinsert;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonAddItem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAddItem = findViewById(R.id.btn_add_home);
+        buttonAddItem.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v==buttonAddItem){
+
+            Intent intent = new Intent(getApplicationContext(),AddItem.class);
+            startActivity(intent);
+        }
+
+    }
+}
